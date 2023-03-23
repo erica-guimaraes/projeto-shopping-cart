eb338204b0534153a66fd93b1489d98c26ad6da3
Érica Guimarães <eriquinha_guima@yahoo.com.br>
2023-03-23 18:27:15 -0300

    Requisitos 4 e 5

--- src/main.js
+++ src/main.js
@@ -3,12 +3,40 @@ import './style.css';
 import { fetchProductsList } from './helpers/fetchFunctions';
 import { createProductElement } from './helpers/shopFunctions';
 
+const product = document.querySelector('.products');
+
+const addLoading = () => {
+  const p = document.createElement('p');
+  p.className = 'loading';
+  p.innerHTML = 'carregando...';
+  product.appendChild(p);
+  return p;
+};
+
+const error = () => {
+  const p = document.createElement('p');
+  p.className = 'error';
+  p.innerHTML = 'Algum erro ocorreu, recarregue a página e tente novamente';
+  product.appendChild(p);
+  return p;
+};
+
+const remove = () => {
+  const loading = document.querySelector('.loading');
+  loading.remove();
+};
+
 const creatList = async () => {
-  const list = await fetchProductsList('computador');
-  const product = document.querySelector('.products');
-  list.forEach((element) => {
-    product.appendChild(createProductElement(element));
-  });
+  addLoading();
+  try {
+    const list = await fetchProductsList('computador');
+    list.forEach((element) => {
+      product.appendChild(createProductElement(element));
+    });
+    remove();
+  } catch {
+    error();
+  }
 };
 creatList();
 
